776b7e0b6d4c5c01d8bfeaef541ccf8bbff8a93b
unknown <lobjakasm@EE-R8RVY6G.groupinfra.com>
2013-06-09 10:03:57 +0300

    IOException missing.

--- src/phd/image/filter/ImageFilter.java
+++ src/phd/image/filter/ImageFilter.java
@@ -1,7 +1,9 @@
 package phd.image.filter;
 
+import java.io.IOException;
+
 import phd.image.Image;
 
 public interface ImageFilter<T extends Image> {
-	T filter(T image); 
+	T filter(T image) throws IOException; 
 }
