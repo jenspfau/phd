074b7e9ae81c9a46c610545496a7f5a01d445554
Jens Pfau <jens.pfau@logica.com>
2013-06-09 11:21:47 +0200

    Fixed OpticalImageTest

--- src/test/java/phd/image/OpticalImageTest.java
+++ src/test/java/phd/image/OpticalImageTest.java
@@ -1,9 +1,12 @@
 package phd.image;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 import org.junit.Test;
 
@@ -13,12 +16,19 @@ public class OpticalImageTest {
 
 	@Test
 	public void test() throws IOException {
-		OpticalImage opticalImage = new OpticalImage();
-		opticalImage.setFile(new File("src/test/resources/testImage.jpg"));
-		opticalImage.setResultFileName("src/test/resources/resultImage.jpg");
+		Coordinates x = new Coordinates(1,3);
+		Coordinates y = new Coordinates(3,4);
+		OpticalImage opticalImage = new OpticalImage(x, y);
+		
+		File file = new File("src/test/resources/testImage.jpg");
+		BufferedImage bufferedImage = ImageIO.read(file);
+		opticalImage.setImage(bufferedImage);
 
 		StreetsFilter filter = new StreetsFilter();
-		filter.filter(opticalImage);
+		OpticalImage resultImage = filter.filter(opticalImage);
+		
+		
+		ImageIO.write(resultImage.getImage(), "jpg", new File("src/test/resources/resultImage.jpg"));
 
 		assertTrue(new File("src/test/resources/resultImage.jpg").exists());
 	}
