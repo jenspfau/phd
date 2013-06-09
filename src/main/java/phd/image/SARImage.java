c4fabe87e284f2e639040f805f9a15974f87d515
mercom <merlin.lobjakas@gmail.com>
2013-06-09 14:34:33 +0300

    SAR image mockup.

--- src/main/java/phd/image/SARImage.java
+++ src/main/java/phd/image/SARImage.java
@@ -1,9 +1,52 @@
 package phd.image;
 
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 public class SARImage extends Image {
+
+
+	private OpticalImage image;
+	private int[][] elevationData;
+
+	private static final String SAR_HEAT_IMAGE_MOCK="src/test/resources/sarHeatImage.jpg";
+	
 	public SARImage(Coordinates x, Coordinates y) {
 		super(x, y);
+		
+		try {
+			mockSAR(SAR_HEAT_IMAGE_MOCK);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
-	private OpticalImage opticalImage;
+	private void mockSAR(String filename) throws IOException {
+		File file = new File(filename);
+		BufferedImage bufferImage = ImageIO.read(file);
+		
+		elevationData = new int[bufferImage.getHeight()][bufferImage.getWidth()];
+		for(int x =0; x < bufferImage.getHeight(); x++){
+			for(int y = 0; y < bufferImage.getWidth(); y++){
+				
+				Color color = new Color(bufferImage.getRGB(y, x));
+				int red = color.getRed();
+				int blue = color.getBlue();
+				
+				int elevation = blue - red;
+				
+				elevationData[x][y] = elevation;
+			}
+		}
+		
+	}
+	
+	public int[][] getElevationData(){
+		return elevationData;
+	}
+
 }
--- src/test/java/phd/image/OpticalImageTest.java
+++ src/test/java/phd/image/OpticalImageTest.java
@@ -3,7 +3,9 @@ package phd.image;
 import static org.junit.Assert.assertTrue;
 
 import java.awt.image.BufferedImage;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
@@ -16,6 +18,7 @@ public class OpticalImageTest {
 
 	private static final String TEST_IMAGE="src/test/resources/testImage.jpg";
 	private static final String RESULT_IMAGE = "src/test/resources/resultImage.jpg";
+	private static final String SAR = "src/test/resources/sarOutput.txt";
 	
 	@Test
 	public void test() throws IOException {
@@ -35,6 +38,22 @@ public class OpticalImageTest {
 		ImageIO.write(resultImage.getImage(), "jpg", resultImageFile);
 
 		assertTrue(resultImageFile.exists());
+		
+		 //SAR image
+	    SARImage sarImage = new SARImage(new Coordinates(0,0), new Coordinates(0, 0));
+	    
+	    /*
+	    FileWriter fstream = new FileWriter(SAR, true);
+        BufferedWriter out = new BufferedWriter(fstream);
+        int[][] elevationInfo = sarImage.getElevationData();
+        for(int i = 0; i < elevationInfo.length; i++ ){
+        	for(int j = 0; j < elevationInfo[i].length; j++){
+        		out.write(elevationInfo[i][j] + " ");
+        	}
+        	out.write("\n");
+        }
+        out.close();
+	    */
 	}
 
 }
